4d1ba5d5018c132854fd37e1968a32dbe8dc04a4
pearl <lu92500@yahoo.com.tw>
2016-03-14 10:38:47 +0800

    Added files via upload

---
+++ src/com/yunos/pay/util/CommonUtil.java
@@ -0,0 +1,30 @@
+package com.yunos.pay.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.util.Log;
+
+
+public class CommonUtil {
+	
+	public static String readRSAKey(InputStream privateKey) {
+		try {
+			String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(privateKey));
+			StringBuffer sb = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+				if (line == "" || (line.length() > 4 && line.substring(0, 5).equals("-----"))) {
+					continue;
+				}
+				sb.append(line);
+			}
+			return sb.toString().trim();
+		} catch (IOException e) {
+			Log.e("error", "InputStream to string error ,", e);
+		}
+		return null;
+	}
+}
