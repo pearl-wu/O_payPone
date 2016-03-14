538cdd31e76a053e2f429315f16789779ceee59d
pearl <lu92500@yahoo.com.tw>
2016-03-14 15:48:12 +0800

    Update yunospay.js

--- www/yunospay.js
+++ www/yunospay.js
@@ -13,8 +13,20 @@ module.exports = {
         options = this.merge(this.ENTER_OPTIONS, options);
           exec(successCallback, errorCallback, "YunOSOrder", "Pay", [options]);
     },
+	
     idchange: function (mag, successCallback, errorCallback) {
         options = this.merge(this.ENTER_OPTIONS, options);
           exec(successCallback, errorCallback, "YunOSOrder", "IdChange", [mag]);
+    },
+	
+	merge: function () {
+        var obj = {};
+        Array.prototype.slice.call(arguments).forEach(function(source) {
+            for (var prop in source) {
+                obj[prop] = source[prop];
+            }
+        });
+        return obj;
     }
+	
 };
