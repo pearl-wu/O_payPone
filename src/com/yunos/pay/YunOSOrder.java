0e9a4a96282b2f87499b353ada3d2f555964d00f
pearl <lu92500@yahoo.com.tw>
2016-03-14 14:20:50 +0800

    Update YunOSOrder.java

--- src/com/yunos/pay/YunOSOrder.java
+++ src/com/yunos/pay/YunOSOrder.java
@@ -7,12 +7,15 @@ import java.util.Date;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,24 +30,33 @@ import com.yunos.pay.util.Config;
 
 public class YunOSOrder extends CordovaPlugin {
 
-	private Button paybutton;
-	private Button unsignButton;
-	private EditText orderIdEdit;
-	private EditText priceEdit;
+	//private Button paybutton;
+	//private Button unsignButton;
+	//private EditText orderIdEdit;
+	//private EditText priceEdit;
 	
-	
+	protected static final String LOG_TAG = "YunOSOrder__pay";
+	private CallbackContext callbackContext = null;
 	
 	
 	public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException{
 		
-		if(action.equals("pay")){
+		if(action.equals("Pay")){
 			//setContentView(R.layout.activity_main);
 			//orderIdEdit = (EditText) findViewById(R.id.orderid);
 			//priceEdit = (EditText) findViewById(R.id.price);
+			
+			final JSONObject options = args.getJSONObject(0);
+				
+			
 			Date dt = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-			orderIdEdit.setText(sdf.format(dt));
+			
+			activity_pay(sdf.format(dt), options);
 
+			return true;
+		}else if(action.equals("IdChange")){
+			
 			//unsignButton = (Button) findViewById(R.id.button1);
 			/*unsignButton.setOnClickListener(new OnClickListener() {
 				@Override
@@ -60,73 +72,85 @@ public class YunOSOrder extends CordovaPlugin {
 	    		}
 			});*/
 			
-			
-			//paybutton = (Button) findViewById(R.id.paybutton);
-			paybutton.setOnClickListener(new OnClickListener() {
+		}
+	
+		return false;
+	}
+	
+	
+	/**********************************產生訂單**********************************************/
+	public String activity_pay(String dt, JSONObject options){		
+		String msg = "false";
+		String subject_id;
+        String subject;
+        String price;
+        String partner_notify_url = null;
+        String partner_order_no;
+		
+		
+		PayClient payer = new PayClient();
+		YunOSOrderManager orderManager = new YunOSOrderManager();
+		
+		
+		try {
+           subject_id = options.getString("subject_id");
+           subject = options.getString("subject");
+           price = options.getString("price");
+           partner_notify_url = options.getString("partner_notify_url");
+           partner_order_no = options.getString("partner_order_no");
+            
+            Log.d(LOG_TAG, "subject_id: " + subject_id);
+        } catch (Exception e) {
+            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
+            result.setKeepCallback(false); // release status callback in JS side
+            callbackContext.sendPluginResult(result);
+            callbackContext = null;
+            return msg;
+        }
 
-				@Override
-				public void onClick(View v) {
-					new AsyncTask<Integer, Object, String>() {
-			            @Override
-			            protected void onPreExecute() {
-			            	paybutton.setClickable(false);
-			            	super.onPreExecute();  
-			            }
-			            @Override
-			            protected String doInBackground(Integer... params) {
-
-							/**********************************產生訂單**********************************************/
-
-
-			            	PayClient payer = new PayClient();
-							YunOSOrderManager orderManager = new YunOSOrderManager();
-							orderManager.GenerateOrder(Config.getPrikey(), Config.getPartner(), "123", "test pay", priceEdit.getText().toString(), Config.getPartnerNotifyUrl(), orderIdEdit.getText().toString());
-							String order = orderManager.getOrder();
-							String sign = orderManager.getSign();
-
-							/***********************************訂單結束*******************************************/
-
-
-							YunOSPayResult payResult = null;
-							String errorMsg = "";
-							String msg = "";
-							Bundle bundle = new Bundle();
-							bundle.putString("provider", "alipay");
-							try {
-								if (payer != null) {
-									payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
-								}
-							} catch (Exception e) {
-								errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
-							}
-							if (payResult != null) {
-								msg = payResult.getPayResult() ? "付款成功" : "付款失敗," + payResult.getPayFeedback();
-							} else {
-								msg = "支付調啟失敗:" + errorMsg;
-							}
-			               return msg;
-			            }
-			            protected void onPostExecute(String result) {
+		orderManager.GenerateOrder(Config.getPrikey(), Config.getPartner(), subject_id, subject, price, Config.getPartnerNotifyUrl(partner_notify_url), dt);
+		String order = orderManager.getOrder();
+		String sign = orderManager.getSign();
+		
+		
+		/***********************************訂單結束*******************************************/
+		YunOSPayResult payResult = null;
+		String errorMsg = "";
+		
+		Bundle bundle = new Bundle();
+		bundle.putString("provider", "alipay");
+		try {
+			if (payer != null) {
+				payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
+			}
+		} catch (Exception e) {
+			errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
+		}
+		if (payResult != null) {
+			msg = payResult.getPayResult() ? "付款成功" : "付款失敗," + payResult.getPayFeedback();
+		} else {
+			msg = "支付調啟失敗:" + errorMsg;
+		}
+		return msg;		
+	}
+	
+	
+	/*
+	 * 			        protected void onPostExecute(String result) {
 			            	Toast.makeText(cordova.getActivity().getApplicationContext(), result, 20).show();
 			            	paybutton.setClickable(true);
 			            	Date dt = new Date();
 							SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
 							orderIdEdit.setText(sdf.format(dt));
 			            };
-			         }.execute(); 
-				}
-			});
-			
-			
-			
-			return true;
-		}
+	*/
 	
-		return false;
-	}
+	
+	
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
 		//getMenuInflater().inflate(R.menu.main, menu);
 		return false;
 	}
+
 }
