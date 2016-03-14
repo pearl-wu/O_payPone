7697e2c30fb442f813c200721250ac7213664b30
pearl <lu92500@yahoo.com.tw>
2016-03-14 16:53:43 +0800

    Update YunOSOrder.java

--- src/com/yunos/pay/YunOSOrder.java
+++ src/com/yunos/pay/YunOSOrder.java
@@ -41,7 +41,9 @@ public class YunOSOrder extends CordovaPlugin {
 			Date dt = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
 			activity_pay(sdf.format(dt), options);
-
+			
+			
+			///Log.i(LOG_TAG, "Pay Pay Pay Pay");
 			return true;
 		}else if(action.equals("IdChange")){
 			boolean boo = args.getBoolean(0);
@@ -69,74 +71,62 @@ public class YunOSOrder extends CordovaPlugin {
 	/**********************************產生訂單**********************************************/
 	public void activity_pay(final String dt, final JSONObject options){	
 		
-		new AsyncTask<Integer, Object, String>() {
 			
 			String msg = "false";
-			String subject_id;
-	        String subject;
-	        String price;
+			String subject_id = null;
+	        String subject = null;
+	        String price = null;
 	        String partner_notify_url = null;
 	        
 	        PayClient payer = new PayClient();
 			YunOSOrderManager orderManager = new YunOSOrderManager();
 			
 			
-			@Override
-            protected void onPreExecute() {
-				try {
-			           subject_id = options.getString("subject_id");
-			           subject = options.getString("subject");
-			           price = options.getString("price");
-			           partner_notify_url = options.getString("partner_notify_url");
-			            
-			           Log.d(LOG_TAG, "subject_id: " + subject_id + "subject:" + subject + "price" + price + "partner_notify_url" + partner_notify_url + "partner_order_no" + dt);
-			            
-			        } catch (Exception e) {
-			            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-			            result.setKeepCallback(false); // release status callback in JS side
-			            callbackContext.sendPluginResult(result);
-			            callbackContext = null;
-			        }     	
-
-            	super.onPreExecute();  
-            }			
+			try {
+		           subject_id = options.getString("subject_id");
+		           subject = options.getString("subject");
+		           price = options.getString("price");
+		           partner_notify_url = options.getString("partner_notify_url");
+		            
+		           Log.d(LOG_TAG, "subject_id: " + subject_id + " , subject:" + subject + " , price:" + price + " , partner_notify_url" + partner_notify_url + " , partner_order_no" + dt);
+		            
+		        } catch (Exception e) {
+		            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
+		            result.setKeepCallback(false); // release status callback in JS side
+		            callbackContext.sendPluginResult(result);
+		            callbackContext = null;
+		        }
+			
+			
+			// TODO Auto-generated method stub
+			orderManager.GenerateOrder(Config.getPrikey(), Config.getPartner(), subject_id, subject, price, Config.getPartnerNotifyUrl(partner_notify_url), dt);
+			String order = orderManager.getOrder();
+			String sign = orderManager.getSign();
+			
+			
+			/***********************************訂單結束*******************************************/
+			/*YunOSPayResult payResult = null;
+			String errorMsg = "";
+			
+			Bundle bundle = new Bundle();
+			bundle.putString("provider", "alipay");
+			try {
+				if (payer != null) {
+					payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
+				}
+			} catch (Exception e) {
+				errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
+			}
+			if (payResult != null) {
+				msg = payResult.getPayResult() ? "付款成功" : "付款失敗," + payResult.getPayFeedback();
+			} else {
+				msg = "支付調啟失敗:" + errorMsg;
+			}
+			
+			Toast.makeText(cordova.getActivity().getApplicationContext(), msg, 20).show();
+			return msg;*/
 		
-			
-			protected String doInBackground(Integer... params) {
-				// TODO Auto-generated method stub
-				orderManager.GenerateOrder(Config.getPrikey(), Config.getPartner(), subject_id, subject, price, Config.getPartnerNotifyUrl(partner_notify_url), dt);
-				String order = orderManager.getOrder();
-				String sign = orderManager.getSign();
-				
-				
-				/***********************************訂單結束*******************************************/
-				YunOSPayResult payResult = null;
-				String errorMsg = "";
-				
-				Bundle bundle = new Bundle();
-				bundle.putString("provider", "alipay");
-				try {
-					if (payer != null) {
-						payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
-					}
-				} catch (Exception e) {
-					errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
-				}
-				if (payResult != null) {
-					msg = payResult.getPayResult() ? "付款成功" : "付款失敗," + payResult.getPayFeedback();
-				} else {
-					msg = "支付調啟失敗:" + errorMsg;
-				}
-				
-				Toast.makeText(cordova.getActivity().getApplicationContext(), msg, 20).show();
-				return msg;
-			};
-			
-			protected void onPostExecute(String result) {
-            	Toast.makeText(cordova.getActivity().getApplicationContext(), result, 20).show();
-            };
-
-		}.execute();
+			//Toast.makeText(cordova.getActivity().getApplicationContext(), result, 20).show();
 
 	}
 	
