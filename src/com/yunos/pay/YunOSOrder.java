5cd437653fcf9d937d45eadd1c0d1b6b4a177ca6
pearl <lu92500@yahoo.com.tw>
2016-03-16 13:35:53 +0800

    Update YunOSOrder.java

--- src/com/yunos/pay/YunOSOrder.java
+++ src/com/yunos/pay/YunOSOrder.java
@@ -10,13 +10,10 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
-import android.widget.Toast;
-
 import com.aliyun.pay.client.PayClient;
 import com.aliyun.pay.client.YunOSPayResult;
 import com.aliyun.pay.order.YunOSOrderManager;
@@ -25,22 +22,21 @@ import com.yunos.pay.util.Config;
 public class YunOSOrder extends CordovaPlugin {
 	
 	protected static final String LOG_TAG = "YunOSOrder__pay";
-	private CallbackContext callbackContext = null;
 	
 	public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException{
 		
 		if(action.equals("Pay")){			
 			final JSONObject options = args.getJSONObject(0);
 			    ///Log.i(LOG_TAG, "Pay Pay Pay Pay");
-			activity_pay(options);
+			activity_pay(options, callbackContext);
 			return true;
 		}else if(action.equals("Change")){
 			boolean boo = args.getBoolean(0);
 				if(!boo){
-					Resultecho(true, "---(IdChange)false---");
+						//Resultecho(true, "---(IdChange)false---", callbackContext);
 					return false;
 				}else{
-					Resultecho(true, "---(IdChange)true---");
+						//Resultecho(true, "---(IdChange)true---", callbackContext);
 					new Thread() {
 						public void run() {
 							Intent intent = new Intent();
@@ -55,7 +51,7 @@ public class YunOSOrder extends CordovaPlugin {
 			boolean tr = args.getBoolean(0);
 			if(tr){
 				String androidId = Secure.getString(cordova.getActivity().getContentResolver(), Secure.ANDROID_ID);
-				Resultecho(true, androidId);
+				Resultecho(true, androidId, callbackContext);
 			}
 			return true;
 		}
@@ -64,20 +60,14 @@ public class YunOSOrder extends CordovaPlugin {
 	
 
 	@SuppressLint("SimpleDateFormat") 
-	public void activity_pay(final JSONObject options){	
+	public void activity_pay(final JSONObject options, CallbackContext callbackContext){	
 		String msg;
 		String partner_order_no = null;
 		String subject_id = null;
 	        String subject = null;
 	        String price = null;
 	        String partner_notify_url = null;
-	        
-	    
-	        PayClient payer = new PayClient();
-		YunOSOrderManager orderManager = new YunOSOrderManager();
-			
-			
-			try {
+	           try {
 		           subject_id = options.getString("subject_id");
 		           subject = options.getString("subject");
 		           price = options.getString("price");
@@ -94,49 +84,50 @@ public class YunOSOrder extends CordovaPlugin {
 		    }
 			
 			
-		    if(partner_order_no == null){
-		        Date dt = new Date();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-			partner_order_no = sdf.format(dt);
+		if(partner_order_no == null){
+		   Date dt = new Date();
+		   SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
+		   partner_order_no = sdf.format(dt);
 	        }
 			
-			/***********************************CREATE**********************************************/
+		/***********************************CREATE**********************************************/
+	        	YunOSOrderManager orderManager = new YunOSOrderManager();
 			orderManager.GenerateOrder(Config.getPrikey(), Config.getPartner(), subject_id, subject, price, Config.getPartnerNotifyUrl(partner_notify_url), partner_order_no);
-			String order = orderManager.getOrder();
-			String sign = orderManager.getSign();
+
 			
-			
-			/***********************************END*******************************************/
+		/***********************************END*******************************************/
 			YunOSPayResult payResult = null;
 			String errorMsg = "";
 			Bundle bundle = new Bundle();
 			bundle.putString("provider", "alipay");
 			
 			try {
-				if (payer != null) {
-					payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
-				}
+			   PayClient payer = new PayClient();
+			   String order = orderManager.getOrder();
+		           String sign = orderManager.getSign();
+			   if (payer != null) {
+				 payResult = payer.YunPay(cordova.getActivity().getBaseContext(), order, sign, bundle);
+			   }
 			} catch (Exception e) {
-				errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
+			   errorMsg = e.getMessage() + "----" + e.getLocalizedMessage();
 			}
-			
 			
 			if (payResult != null) {
-				msg = payResult.getPayResult() ? "PAY success " : "PAY error," + payResult.getPayFeedback();
+			   msg = payResult.getPayResult() ? "PAY success " : "PAY error," + payResult.getPayFeedback();
 			} else {
-				msg = "(YunPay)ERROR:" + errorMsg;
+			   msg = "(YunPay)ERROR:" + errorMsg;
 			}
 			
-			Resultecho(true, msg);
+		Resultecho(true, msg, callbackContext);
 	}
 	
-	public void Resultecho(Boolean boo, String meg){
+	public void Resultecho(Boolean boo, String meg, CallbackContext callbackContext){
 	   if(boo){
-		Toast.makeText(cordova.getActivity(), meg, Toast.LENGTH_SHORT).show();
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, meg));
-            } else {
-            	Toast.makeText(cordova.getActivity(), meg, Toast.LENGTH_LONG).show();
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, meg));
-	    }			
+		//Toast.makeText(cordova.getActivity(), meg, Toast.LENGTH_SHORT).show();
+            	callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, meg));
+	   } else {
+	        //Toast.makeText(cordova.getActivity(), meg, Toast.LENGTH_LONG).show();
+	        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, meg));
+	   }			
 	}	
 }
