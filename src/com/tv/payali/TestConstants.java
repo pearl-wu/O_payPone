0f4b889ae3e3ae188a6740a023647ae42cd5043d
pearl <lu92500@yahoo.com.tw>
2016-08-19 10:33:58 +0800

    Update TestConstants.java

--- src/com/tv/payali/TestConstants.java
+++ src/com/tv/payali/TestConstants.java
@@ -2,9 +2,9 @@ package com.tv.payali;
 
 public class TestConstants {
 
-    public static final String APPKEY = "23363370";
-    public static final String APP_SECRET = "f9b0baca3bf586b7476f74fc0e71a719";
-    public static final String BAODIAN_SECRET_KEY = "99fe24fb59a793641d5f6d6942960b75";
+    public static final String APPKEY = "";
+    public static final String APP_SECRET = "";
+    public static final String BAODIAN_SECRET_KEY = "";
  
 //    public static final String APPKEY = "618698";
 //    public static final String APP_SECRET = "c133ad8aed62f00906762516499ad731";
